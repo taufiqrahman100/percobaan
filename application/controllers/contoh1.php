87eeed40a7b53f10a71d48130af236fc1342f7eb
taufiqrahman100 <rtaufiq203@gmail.com>
2021-09-30 19:36:28 +0700

    Mengerjakkan Modul Pertemuan 3

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Taufiq Rahman Noor Roby.
+        Saya tinggal di Jl.Tanuwijaya RT/RW 002/005 kelurahan Empangsari, kecamatan Tawang.
+        Olahraga kesukaan saya adalah sepak bola dan futsal.
+        Nomor Handphone 081295006529";
+    }
+}
+?>
---
+++ application/views/view-latihan1.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+    <title>latihan 1</title>
+</head>
+
+<body>
+    Halo kawan.. Yuk kita belajar web programming..!!!<br>
+    nilai 1 = <?= $nilai1; ?><p>
+    nilai 2 = <?= $nilai2; ?><p>
+    ini hasil dari permodelan dengan metode penjumlahan yaitu <?=
+    $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+
+</body>
+
+</html>
